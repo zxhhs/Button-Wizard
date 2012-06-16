e3fa08515bbaaa5c6e17c31f5ad6ef6db5a4de79
zgp1990 <zgp@zgp-PC.(none)>
2012-06-16 23:34:43 +0800

    last code

--- Z_do.c
+++ Z_do.c
@@ -222,14 +222,204 @@ int zdo_click(int button, int repeat, unsigned long delay) {
 
 
 void addListener(Display* display, Window r) {
+    //get children list
+    unsigned int listnum;
+    Window parent;
+    Window root;
+    Window *wa;
+
+    XQueryTree(display, r, &root, &parent, &wa, &listnum);
+    
+    XGrabKeyboard(display, root, True, GrabModeAsync, GrabModeAsync, CurrentTime );
+    XGrabPointer(display, root, True, PointerMotionMask|ButtonPressMask, 
+    		GrabModeAsync, GrabModeAsync, None, None, CurrentTime);
+    return;
+    
+    /*if (wa == NULL) {
+        return;
+    }
+    int i=0;
+
+    for (i=0; i<listnum; i++) {
+        Window w = wa[i];
+
+        XWindowAttributes tattributes;
+        XGetWindowAttributes(display, w, &tattributes);
+ 
+        if (tattributes.map_state == 2) { //visible
+        	XGrabKeyboard(display, w, True, GrabModeAsync, GrabModeAsync, CurrentTime );
+            XGrabPointer(display, w, True, PointerMotionMask|ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None, CurrentTime);
+        }
+        addListener(display, w);
+    }*/
 }
 
 int waitForSpace() {
-	return 1;
+	fd_set rfds, rs;
+	struct timeval tv;
+
+	int i, r, j;
+	struct termios saveterm, nt;
+	int fd = 0;
+	unsigned char buf[32];
+
+	tcgetattr(fd, &saveterm);
+	nt = saveterm;
+
+	nt.c_lflag &= ~ECHO;      
+	nt.c_lflag &= ~ISIG;      
+	nt.c_lflag &= ~ICANON;    
+
+	tcsetattr(fd, TCSANOW, &nt);
+
+	FD_ZERO(&rs);
+	FD_SET(fd, &rs);
+	tv.tv_sec = 0;
+	tv.tv_usec = 0;
+
+	i = 0;
+	zdo_alert('g', "\npress <Space> to start record.\n");
+	zdo_alert('g', "press <Esc> to end.\n");
+	while (1) {
+		read(fd, buf+i, 1);
+		i++;		
+		r = select(fd+1, &rfds, NULL, NULL, &tv);    
+		if (r < 0)
+		{
+		   zdo_alert('r', "select error\n");
+		   return ZDO_ERROR;
+		}
+		if(r == 1)
+			return ZDO_ERROR;
+
+		rfds = rs;
+		for(j=0; j<i; j++)
+		{
+		   switch (buf[j])
+		   {
+			case 32 : //Space
+				tcsetattr(fd, TCSANOW, &saveterm);
+				return ZDO_SUCCESS;
+			case 27 : //esc
+				tcsetattr(fd, TCSANOW, &saveterm);
+				return ZDO_ERROR;
+			default :
+				break;
+			}
+		}
+	}
 }
 
 int zdo_record(char *file, int is_continue, int mouse_mask, int key_mask) {
-	return 1;
+	FILE *output = NULL;
+	clock_t start, finish;
+	double duration;
+	
+	if (!is_continue)
+		output = fopen(file, "w");
+	else 
+		output = fopen(file, "a");
+		
+	if (output == NULL) {
+		zdo_alert('r', "Open File Error.\n");
+		return ZDO_ERROR;
+	}
+	
+	
+	while (waitForSpace() == ZDO_SUCCESS) {
+		Display* disp = XOpenDisplay( NULL );          
+		Screen *screen=XDefaultScreenOfDisplay(disp);
+		Window window=XRootWindowOfScreen(screen);
+
+		addListener(disp, window);
+	
+		start = clock();
+		zdo_alert('g', "\nRecord Started.\n");
+		zdo_alert('g', "Press <Esc> to pause record.\n");
+		while (1) {
+		    if (XPending(disp)) {
+		        XEvent e;
+		        XNextEvent(disp, &e);
+		        
+		        //keyborad event
+		        if (e.type == KeyPress) {
+		        	duration = (double)(clock() - start) / CLOCKS_PER_SEC;
+	        		fprintf(output, "./ZMagic sleep %f\n", duration);
+	            	start = clock();
+		        
+		            XKeyEvent key = e.xkey;
+		            unsigned int keycode = key.keycode;
+		            unsigned int state = key.state;
+		            char *str = (char*)calloc(20, sizeof(char));
+		            KeySym keysym;
+		            
+		            switch (state) {
+		            case 0:
+		            case 1:
+		            	keysym = XKeycodeToKeysym(disp, keycode, state);
+		            	str = XKeysymToString(keysym);
+		            	break;
+		            case 4:
+		            	keysym = XKeycodeToKeysym(disp, keycode, 0);
+		            	sprintf(str, "Ctrl+%s", XKeysymToString(keysym));
+		            	break;
+		            case 8:
+		            	keysym = XKeycodeToKeysym(disp, keycode, 0);
+		            	sprintf(str, "Alt+%s", XKeysymToString(keysym));
+		            	break;
+		            case 64:
+		            	keysym = XKeycodeToKeysym(disp, keycode, 0);
+		            	sprintf(str, "Super+%s", XKeysymToString(keysym));
+		            	break;
+		            default:
+		            	keysym = XKeycodeToKeysym(disp, keycode, 0);
+		            	str = XKeysymToString(keysym);
+		            	break;
+		            }
+		            if (!strcmp(str, "Escape"))
+		            	break;
+
+					if (!mouse_mask)
+		            	fprintf(output, "./ZMagic mousemove %d %d\n", key.x_root, key.y_root);
+		            if (!key_mask) {
+				        fprintf(output, "./ZMagic key %s\n", str);
+				        zdo_alert('b', "key:%s - x:%d, y:%d\n", str, key.x_root, key.y_root);
+		            }
+		        } 
+		        //mouse click event
+		        else if (!mouse_mask && e.type == ButtonPress){
+		        	duration = (double)(clock() - start) / CLOCKS_PER_SEC;
+	        		fprintf(output, "./ZMagic sleep %f\n", duration);
+	            	start = clock();
+	            	
+		            XButtonEvent xbutton=e.xbutton;
+		            fprintf(output, "./ZMagic mousemove %d %d\n", xbutton.x, xbutton.y);
+		            fprintf(output, "./ZMagic click %d\n", xbutton.button);
+		            
+		            zdo_alert('b', "mouse-click:%d - x:%d, y:%d\n", xbutton.button, xbutton.x, xbutton.y);
+		        }
+		        //mouse move event
+		        else if (!mouse_mask && e.type == MotionNotify) {
+		        	duration = (double)(clock() - start) / CLOCKS_PER_SEC;
+		        	if (duration > 0.2) {
+		        		fprintf(output, "./ZMagic sleep %f\n", duration);
+		        		fprintf(output, "./ZMagic mousemove %d %d\n", e.xmotion.x_root, e.xmotion.y_root);
+		            	zdo_alert('b', "mouse-move - x:%d, y:%d\n", e.xmotion.x_root, e.xmotion.y_root);
+		            	start = clock();
+		        	}
+		        }
+		    }
+		}
+		
+		XUngrabPointer(disp, CurrentTime);
+		XUngrabKeyboard(disp, CurrentTime);
+		XCloseDisplay(disp);
+    }
+    
+    if (chmod(file, 0777) == -1) {
+    	zdo_alert('r', "chmod error.\n");
+    }
+    fclose(output);
   
 }
 
--- cmd_record.c
+++ cmd_record.c
@@ -6,7 +6,86 @@
 #include <getopt.h>
 
 int cmd_record(context_t *context) {
+	int option_index;
+	char *cmd = *context->argv;
+	char c;
+	int is_continue = 0;
+	int key_mask = 0;
+	int mouse_mask = 0;
+	char *file;
 	
+	static const char *usage =
+    "\x1b[1;32mUsage:\x1b[0m \x1b[31m%s\x1b[0m \x1b[1;34m[--mouse_only] [--key_only] [--continue] [--help] <file>\x1b[0m \n"
+    "\x1b[32mRecord the motions of mouse and keyboard into the file(should be *.sh).\x1b[0m\n"
+    "\x1b[1;34m--mouse_only -m\x1b[0m        \x1b[32m- record mouse movements only.\x1b[0m\n"
+    "\x1b[1;34m--key_only   -k\x1b[0m        \x1b[32m- record keyboard movements only.\x1b[0m\n"
+    "\x1b[1;34m--continue   -c\x1b[0m        \x1b[32m- write records from the end of the file(must exists).\x1b[0m\n"
+    "\x1b[1;34m--help       -h\x1b[0m        \x1b[32m- get help\x1b[0m\n";
+	
+	static struct option longopts[] = {
+		{ "mouse_only", no_argument, NULL, 'm'},
+		{ "key_only", no_argument, NULL, 'k'},
+		{ "continue", no_argument, NULL, 'c'},
+		{ "help", no_argument, NULL, 'h'},
+		{ 0, 0, 0, 0 },
+	};
+	
+	while ((c = getopt_long_only(context->argc, context->argv, "mkhc",
+                               longopts, &option_index)) != -1) {
+		switch (c) {
+			case 'm':
+				key_mask = 1;
+				mouse_mask = 0;
+				break;
+			case 'k':
+				key_mask = 0;
+				mouse_mask = 1;
+				break;
+			case 'c':
+				is_continue = 1;
+				break;
+			case 'h':
+				printf(usage, cmd);
+				return ZDO_SUCCESS;
+				break;
+		  	default:
+				zdo_alert('r', "unknown option.\n");
+				fprintf(stderr, usage, cmd);
+				return ZDO_ERROR;
+		}
+	}
+	consume_args(context, optind);
+	
+	if (context->argc != 0) {
+		file = context->argv[0];
+		if (!is_continue && access(file, 0) != -1) {
+			//file exists
+			zdo_alert('r', "The %s exists. Do you want to overwrite it? (y/c/n)\n", file);
+			zdo_alert('g', "--select 'y' to overwrite.\n");
+			zdo_alert('g', "--select 'c' to continue.\n");
+			zdo_alert('g', "--select 'n' to quit.\n");
+
+			scanf("%c", &c);
+			if (c == 'y') {
+				is_continue = 0;
+			}
+			else if (c == 'c') {
+				is_continue = 1;
+			}
+			else {
+				return ZDO_SUCCESS;
+			}
+		}
+		else if (is_continue && access(file, 0) == -1) {
+			zdo_alert('r', "No such file(%s).\n", file);
+			return ZDO_ERROR;
+		}
+		zdo_record(file, is_continue, mouse_mask, key_mask);
+	}
+	else {
+		zdo_alert('r', "Need a file for record.\n");
+		printf(usage, cmd);	
+	}
 	return ZDO_SUCCESS;
 }
 
